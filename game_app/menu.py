35638730f4b90b45f3148fdad86300160cec078f
Yoshie <yoshuaaa7@gmail.com>
2025-04-02 23:23:19 +0200

    Created basics of app main menu

--- game_app/menu.py
+++ game_app/menu.py
@@ -1,24 +1,44 @@
 import tkinter as tk
+from tkinter import font
 from game_app import start_snake, start_tetris, start_pong
 
 def show_menu():
     for widget in root.winfo_children():
         widget.destroy()
 
-    label = tk.Label(root, text="Wybierz grę", font=("Arial", 14))
-    label.pack(pady=10)
+    # Create Canva on the entire app window
+    canvas = tk.Canvas(root, width=800, height=600, highlightthickness=0)
+    canvas.pack()
 
-    btn_snake = tk.Button(root, text="Snake", command=show_snake, width=20)
-    btn_snake.pack(pady=5)
+    # Set image as a background
+    canvas.create_image(app_center_x, app_center_y, image=background_image)
 
-    btn_tetris = tk.Button(root, text="Tetris", command=show_tetris, width=20)
-    btn_tetris.pack(pady=5)
+    # Game title
+    canvas.create_text(app_center_x, 64, text="Retro Games\nCollections", font=nunito_bold, fill="#361706", justify="center")
 
-    btn_pong = tk.Button(root, text="Pong", command=show_pong, width=20)
-    btn_pong.pack(pady=5)
+    # Choose game label
+    line_length = 150
+    canvas.create_line(100, 170, 100 + line_length, 170, fill="#361706", width=3)
+    canvas.create_text(app_center_x, 170, text="Choose a game to play", font=nunito_regular, fill="#361706",
+                       justify="center")
+    canvas.create_line(700 - line_length, 170, 700, 170, fill="#361706", width=3)
 
-    btn_exit = tk.Button(root, text="Wyjdź", command=root.quit, width=20)
-    btn_exit.pack(pady=10)
+
+
+    # label = tk.Label(root, text="Wybierz grę", font=("Arial", 14))
+    # label.pack(pady=10)
+    #
+    # btn_snake = tk.Button(root, text="Snake", command=show_snake, width=20)
+    # btn_snake.pack(pady=5)
+    #
+    # btn_tetris = tk.Button(root, text="Tetris", command=show_tetris, width=20)
+    # btn_tetris.pack(pady=5)
+    #
+    # btn_pong = tk.Button(root, text="Pong", command=show_pong, width=20)
+    # btn_pong.pack(pady=5)
+    #
+    # btn_exit = tk.Button(root, text="Wyjdź", command=root.quit, width=20)
+    # btn_exit.pack(pady=10)
 
 def show_snake():
     for widget in root.winfo_children():
@@ -60,6 +80,13 @@ root = tk.Tk()
 root.title("Retro Games")
 root.geometry("800x600")
 root.resizable(False, False)
+
+app_center_x = 400
+app_center_y = 300
+
+background_image = tk.PhotoImage(file="media/menuBackground.png")
+nunito_bold = font.Font(family="Nunito", size=28, weight="bold")
+nunito_regular = font.Font(family="Nunito", size=18, weight="bold")
 
 show_menu()
 
