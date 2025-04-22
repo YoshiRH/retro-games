5868556b80aaa1a4164c1b4624f2b2e52bd82c92
xmilek <145913047+xmilek@users.noreply.github.com>
2025-04-22 19:09:47 +0200

    12 Add basic game board

--- game_app/snake/snake.py
+++ game_app/snake/snake.py
@@ -7,8 +7,13 @@ def main(root):
     pygame.init()
     os.environ['SDL_WINDOWID'] = str(root.winfo_id())
     pygame.display.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800,600))
     clock = pygame.time.Clock()
+
+    grid_box_size = 35
+
+    game_board_border = pygame.Rect(0,112,800,488)
+    game_board = pygame.Rect(34, 145, (21 * grid_box_size), (12 * grid_box_size))
 
     #Main game loop
     running = True
@@ -19,7 +24,10 @@ def main(root):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-        screen.fill("#D3E671")
+
+        screen.fill("#89ac46")
+        pygame.draw.rect(screen, "#4d6127", game_board_border)
+        pygame.draw.rect(screen, "#d3e671", game_board)
 
         pygame.display.update()
         clock.tick(60)
